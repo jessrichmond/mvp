7882a6abe5eb18fd291475e294ae080f3133cfef
Jess Richmond <yesjessrichmond@gmail.com>
2020-09-21 14:01:59 -0500

    connection between db & server

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,21 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const { PORT } = require('./.env')
+const db = require('./db')
 
 app.listen(PORT, () => {
-  console.log(`bulletin listening at http://localhost:${PORT}`);
+  console.log(`!! CONNECTED TO SERVER @ PORT ${PORT} !!`);
 });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.log('not listening to database');
+//   } else {
+//     app.listen(PORT, () => {
+//       console.log(`bulletin listening at http://localhost:${PORT}`);
+//     });
+//   }
+// });
 
 app.get('/', (req, res) => {
   res.send('hellloooooo');
