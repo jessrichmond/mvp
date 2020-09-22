a04015ef7b0261d12da9f0ce1145c81f456d4e82
Jess Richmond <yesjessrichmond@gmail.com>
2020-09-22 15:44:13 -0500

    well i'll be g*dd*mned - sequelize *actually* working, table being created in db

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,4 @@
-// const mysql = require('mysql');
-const Sequelize = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
 const DB_NAME = 'bulletin_app';
 const DB_USER = 'root';
@@ -7,64 +6,22 @@ const DB_PASS = '';
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
   dialect: 'mysql',
-})
-
-const User = sequelize.define('User', {
-  user_id: Sequelize.NUMBER,
-  username: Sequelize.STRING,
-  password: Sequelize.STRING,
-  location: Sequelize.NUMBER
+  logging: false
 })
 
 const Entry = sequelize.define('Entry', {
-  message_id: Sequelize.NUMBER,
-  user_id: Sequelize.NUMBER,
+  messageId: Sequelize.INTEGER,
   body: Sequelize.STRING,
-  date: Sequelize.DATE
 })
 
-sequelize.authenticate()
-  .then(() => console.log('sequelize running, db connected'))
-  .catch((err) => console.log('sequelize auth failed: ', err))
+sequelize.sync({ force: true })
+  .then(() => console.log('database & tables created'))
+  .catch((err) => console.log('err in sync', err))
 
 
 module.exports = {
   // connection
-  User,
   Entry
 };
-
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '',
-//   database: 'bulletin'
-// })
-
-
-// connection.connect(() => {
-//   return Promise.resolve()
-//     .then(() => console.log('!! CONNECTED TO MYSQL !!'))
-//     .then(() => connection.query(`CREATE DATABASE IF NOT EXISTS bulletin`))
-//     .then(() => {connection.query(
-//       `CREATE TABLE IF NOT EXISTS users (
-//         user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-//         username VARCHAR(15) UNIQUE,
-//         password VARCHAR(64),
-//         location INT(5)
-//       )`
-//     )})
-//     .then(() => {connection.query(
-//       `CREATE TABLE IF NOT EXISTS messages (
-//         message_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-//         user_id INT,
-//         body VARCHAR(255),
-//         date DATE
-//       )`
-//     )})
-//     .then(() => {
-//       connection.end();
-//     })
-// })
 
 
--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const { PORT } = require('./.env')
-const db = require('./db')
+const { Entry } = require('./db')
 const path = require('path');
 const bodyParser = require('body-parser');
 
 const DIST_DIR = path.join(__dirname, '../dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 const datamuse = 'https://api.datamuse.com/words?';
-const { User, Entry } = require('./models');
-
+   
 app.use(bodyParser.json());
 app.use(express.static(DIST_DIR));
 
@@ -19,15 +18,15 @@ app.use(express.static(DIST_DIR));
 // })
 
 
-app.get('/signup', (req, res) => {
-  res.status(200).send('get signup!');
-  // res.render('signup');
+app.post('/signup', (req, res) => {
+  const { username } = req.body;
+  res.send('hello');
 })
 
-app.post('/signup', (req, res) => {
-  const { username, password } = req.body;
-  res.send(req.body);
-})
+// app.post('/signup', (req, res) => {
+//   const { username, password } = req.body;
+//   return User.create({ username, password});
+// })
 
 
 app.listen(PORT, () => {
